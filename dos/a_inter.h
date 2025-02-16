ad7620417f11ffa59f25c5ac1b5554b70225f1d8
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-02-16 12:19:26 +0100

    remove Watcom and 16-bit code

--- dos/a_inter.h
+++ dos/a_inter.h
@@ -29,11 +29,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
    (c) Copyright 1994 James R. Dose.  All Rights Reserved.
 **********************************************************************/
 
-static uint32_t DisableInterrupts(void);
-static void RestoreInterrupts(uint32_t flags);
-
-
-#if defined __DJGPP__
 static uint32_t DisableInterrupts(void)
 {
 	uint32_t a;
@@ -57,28 +52,3 @@ static void RestoreInterrupts(uint32_t flags)
 		: "r" (flags)
 	);
 }
-
-#elif defined __WATCOMC__ && defined _M_I386
-#pragma aux DisableInterrupts =	\
-	"pushfd",					\
-	"pop eax",					\
-	"cli"						\
-	value [eax];
-
-#pragma aux RestoreInterrupts =	\
-	"push eax",					\
-	"popfd"						\
-	parm [eax];
-#else
-static uint32_t DisableInterrupts(void)
-{
-	_disable();
-	return 0;
-}
-
-static void RestoreInterrupts(uint32_t flags)
-{
-	UNUSED(flags);
-	_enable();
-}
-#endif
